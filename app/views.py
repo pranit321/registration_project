a8a9cdbe801161419814454d60248742ec7b77ef
pranit <pranitsahu2000@gmail.com>
2024-01-23 11:45:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,9 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 from app.models import *
 from app.forms import *
+from django.http import HttpResponse
 
 def registration(request):
     usfo=UserForm()
     pfo=ProfileForm()
     d={'usfo':usfo,'pfo':pfo}
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            MUFDO.set_password(ufd.cleaned_data['password'])
+            MUFDO.save()
+            MPFD=pfd.save(commit=False)
+            MPFD.Usernames=MUFDO
+            MPFD.save()
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not')
+
+
     return render(request,'registration_form.html',d)
--- registration_project/urls.py
+++ registration_project/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from app.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
-]
+
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
